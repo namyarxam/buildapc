b833fed1e1b9c25df12c10934473a3cd81c7933b
Max Rayman <mxray333@gmail.com>
2016-04-06 10:00:05 -0400

    seed/schema inserted

--- db/schema.sql
+++ db/schema.sql
@@ -13,23 +13,135 @@ DROP TABLE IF EXISTS keyboards CASCADE;
 DROP TABLE IF EXISTS mice CASCADE;
 DROP TABLE IF EXISTS speakers CASCADE;
 
-CREATE TABLE users (
-  user_id SERIAL UNIQUE PRIMARY KEY,
-  email VARCHAR(255) NOT NULL,
-  password_digest TEXT NOT NULL,
-  build_id INT REFERENCES builds(build_id)
-)
+CREATE TABLE processors (
+  processor_id SERIAL UNIQUE PRIMARY KEY,
+  name TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  store TEXT NOT NULL,
+  price INT NOT NULL,
+  description TEXT NOT NULL,
+  buy_link VARCHAR(255)
+);
+
+CREATE TABLE memory (
+  memory_id SERIAL UNIQUE PRIMARY KEY,
+  name TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  store TEXT NOT NULL,
+  price INT NOT NULL,
+  description TEXT NOT NULL,
+  buy_link VARCHAR(255)
+);
+
+CREATE TABLE hds (
+  hd_id SERIAL UNIQUE PRIMARY KEY,
+  name TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  store TEXT NOT NULL,
+  price INT NOT NULL,
+  description TEXT NOT NULL,
+  buy_link VARCHAR(255)
+);
+
+CREATE TABLE gpus (
+  gpu_id SERIAL UNIQUE PRIMARY KEY,
+  name TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  store TEXT NOT NULL,
+  price INT NOT NULL,
+  description TEXT NOT NULL,
+  buy_link VARCHAR(255)
+);
+
+CREATE TABLE motherboards (
+  motherboard_id SERIAL UNIQUE PRIMARY KEY,
+  name TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  store TEXT NOT NULL,
+  price INT NOT NULL,
+  description TEXT NOT NULL,
+  buy_link VARCHAR(255)
+);
+
+CREATE TABLE psus (
+  psu_id SERIAL UNIQUE PRIMARY KEY,
+  name TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  store TEXT NOT NULL,
+  price INT NOT NULL,
+  description TEXT NOT NULL,
+  buy_link VARCHAR(255)
+);
+
+CREATE TABLE cases (
+  case_id SERIAL UNIQUE PRIMARY KEY,
+  name TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  store TEXT NOT NULL,
+  price INT NOT NULL,
+  description TEXT NOT NULL,
+  buy_link VARCHAR(255)
+);
+
+CREATE TABLE monitors (
+  monitor_id SERIAL UNIQUE PRIMARY KEY,
+  name TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  store TEXT NOT NULL,
+  price INT NOT NULL,
+  description TEXT NOT NULL,
+  buy_link VARCHAR(255)
+);
+
+CREATE TABLE headphones (
+  headphone_id SERIAL UNIQUE PRIMARY KEY,
+  name TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  store TEXT NOT NULL,
+  price INT NOT NULL,
+  description TEXT NOT NULL,
+  buy_link VARCHAR(255)
+);
+
+CREATE TABLE keyboards (
+  keyboard_id SERIAL UNIQUE PRIMARY KEY,
+  name TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  store TEXT NOT NULL,
+  price INT NOT NULL,
+  description TEXT NOT NULL,
+  buy_link VARCHAR(255)
+);
+
+CREATE TABLE mice (
+  mouse_id SERIAL UNIQUE PRIMARY KEY,
+  name TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  store TEXT NOT NULL,
+  price INT NOT NULL,
+  description TEXT NOT NULL,
+  buy_link VARCHAR(255)
+);
+
+CREATE TABLE speakers (
+  speaker_id SERIAL UNIQUE PRIMARY KEY,
+  name TEXT NOT NULL,
+  img_url VARCHAR(255) NOT NULL,
+  store TEXT NOT NULL,
+  price INT NOT NULL,
+  description TEXT NOT NULL,
+  buy_link VARCHAR(255)
+);
 
 CREATE TABLE builds (
   build_id SERIAL UNIQUE PRIMARY KEY,
   name VARCHAR(25) NOT NULL,
-  cpu_id INT REFERENCES processors(cpu_id) NOT NULL,
+  cpu_id INT REFERENCES processors(processor_id) NOT NULL,
   memory_id INT REFERENCES memory(memory_id) NOT NULL,
   hd_id INT REFERENCES hds(hd_id) NOT NULL,
   ssd BOOLEAN NOT NULL,
   gpu_id INT REFERENCES gpus(gpu_id) NOT NULL,
   motherboard_id INT REFERENCES motherboards(motherboard_id) NOT NULL,
-  cooler_id INT REFERENCES coolers(cooler_id),
   psu_id INT REFERENCES psus(psu_id),
   case_id INT REFERENCES cases(case_id),
   optical_drive BOOLEAN NOT NULL, 
@@ -38,124 +150,11 @@ CREATE TABLE builds (
   keyboard_id INT REFERENCES keyboards(keyboard_id),
   mouse_id INT REFERENCES mice(mouse_id),
   speaker_id INT REFERENCES speakers(speaker_id)
-)
+);
 
-CREATE TABLE processors (
-  processor_id SERIAL UNIQUE PRIMARY KEY,
-  name TEXT NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
-  store TEXT NOT NULL,
-  price INT NOT NULL,
-  description TEXT NOT NULL
-  buy_link VARCHAR(255)
-)
-
-CREATE TABLE memory (
-  memory_id SERIAL UNIQUE PRIMARY KEY,
-  name TEXT NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
-  store TEXT NOT NULL,
-  price INT NOT NULL,
-  description TEXT NOT NULL
-  buy_link VARCHAR(255)
-)
-
-CREATE TABLE hds (
-  hd_id SERIAL UNIQUE PRIMARY KEY,
-  name TEXT NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
-  store TEXT NOT NULL,
-  price INT NOT NULL,
-  description TEXT NOT NULL
-  buy_link VARCHAR(255)
-)
-
-CREATE TABLE gpus (
-  gpu_id SERIAL UNIQUE PRIMARY KEY,
-  name TEXT NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
-  store TEXT NOT NULL,
-  price INT NOT NULL,
-  description TEXT NOT NULL
-  buy_link VARCHAR(255)
-)
-
-CREATE TABLE motherboards (
-  motherboard_id SERIAL UNIQUE PRIMARY KEY,
-  name TEXT NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
-  store TEXT NOT NULL,
-  price INT NOT NULL,
-  description TEXT NOT NULL
-  buy_link VARCHAR(255)
-)
-
-CREATE TABLE psus (
-  psu_id SERIAL UNIQUE PRIMARY KEY,
-  name TEXT NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
-  store TEXT NOT NULL,
-  price INT NOT NULL,
-  description TEXT NOT NULL
-  buy_link VARCHAR(255)
-)
-
-CREATE TABLE cases (
-  case_id SERIAL UNIQUE PRIMARY KEY,
-  name TEXT NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
-  store TEXT NOT NULL,
-  price INT NOT NULL,
-  description TEXT NOT NULL
-  buy_link VARCHAR(255)
-)
-
-CREATE TABLE monitors (
-  monitor_id SERIAL UNIQUE PRIMARY KEY,
-  name TEXT NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
-  store TEXT NOT NULL,
-  price INT NOT NULL,
-  description TEXT NOT NULL
-  buy_link VARCHAR(255)
-)
-
-CREATE TABLE headphones (
-  headphone_id SERIAL UNIQUE PRIMARY KEY,
-  name TEXT NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
-  store TEXT NOT NULL,
-  price INT NOT NULL,
-  description TEXT NOT NULL
-  buy_link VARCHAR(255)
-)
-
-CREATE TABLE keyboards (
-  keyboard_id SERIAL UNIQUE PRIMARY KEY,
-  name TEXT NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
-  store TEXT NOT NULL,
-  price INT NOT NULL,
-  description TEXT NOT NULL
-  buy_link VARCHAR(255)
-)
-
-CREATE TABLE mice (
-  mouse_id SERIAL UNIQUE PRIMARY KEY,
-  name TEXT NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
-  store TEXT NOT NULL,
-  price INT NOT NULL,
-  description TEXT NOT NULL
-  buy_link VARCHAR(255)
-)
-
-CREATE TABLE speakers (
-  speaker_id SERIAL UNIQUE PRIMARY KEY,
-  name TEXT NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
-  store TEXT NOT NULL,
-  price INT NOT NULL,
-  description TEXT NOT NULL
-  buy_link VARCHAR(255)
-)
+CREATE TABLE users (
+  user_id SERIAL UNIQUE PRIMARY KEY,
+  email VARCHAR(255) NOT NULL,
+  password_digest TEXT NOT NULL,
+  build_id INT REFERENCES builds(build_id)
+);
--- db/seed.sql
+++ db/seed.sql
@@ -1,132 +1,123 @@
-
-DROP TABLE IF EXISTS psus CASCADE;
-DROP TABLE IF EXISTS cases CASCADE;
-DROP TABLE IF EXISTS monitors CASCADE;
-DROP TABLE IF EXISTS headphones CASCADE;
-DROP TABLE IF EXISTS keyboards CASCADE;
-DROP TABLE IF EXISTS mice CASCADE;
-DROP TABLE IF EXISTS speakers CASCADE;
-
 -- PROCESSORS
 INSERT INTO processors(name, img_url, store, price, description, buy_link) VALUES ('Intel
 Core i5-4590', 'http://i.imgur.com/ava6Dvn.jpg', 'Micro Center', '160', 'Same 
-processor speed as the fastest iMac on the market. Very quick, good for casual/non gamers.', 'http://www.microcenter.com/product/432161/Core_i5-4590_33GHz_LGA_1150_Boxed_Processor')
+processor speed as the fastest iMac on the market. Very quick, good for casual/non gamers.', 'http://www.microcenter.com/product/432161/Core_i5-4590_33GHz_LGA_1150_Boxed_Processor');
 INSERT INTO processors(name, img_url, store, price, description, buy_link) VALUES ('Intel
 Core i5-4690K', 'http://i.imgur.com/ava6Dvn.jpg', 'Micro Center', '200', 'Extremely fast 
-processor that works great for nearly all high-end gaming needs.', 'http://www.microcenter.com/product/434177/Core_i5-4690K_35GHz_LGA_1150_Boxed_Processor')
+processor that works great for nearly all high-end gaming needs.', 'http://www.microcenter.com/product/434177/Core_i5-4690K_35GHz_LGA_1150_Boxed_Processor');
 INSERT INTO processors(name, img_url, store, price, description, buy_link) VALUES ('Intel
 Core i7-4790K', 'http://i.imgur.com/zfQu7WC.jpg', 'Micro Center', '290', 'Overkill in terms of 
-gaming. If you want to live-stream gaming with a dual-monitor setup, this is probably your best bet.', 'http://www.microcenter.com/product/434176/Core_i7-4790K_40GHz_LGA_1150_Boxed_Processor')
+gaming. If you want to live-stream gaming with a dual-monitor setup, this is probably your best bet.', 'http://www.microcenter.com/product/434176/Core_i7-4790K_40GHz_LGA_1150_Boxed_Processor');
 
 -- MEMORY
 INSERT INTO memory(name, img_url, store, price, description, buy_link) VALUES ('HyperX Fury 8GB', 
 'http://i.imgur.com/Bv4LITa.jpg', 'Newegg', '44', 'Standard memory for personal desktops. Plenty of room for background
-applications.', 'http://www.newegg.com/Product/Product.aspx?Item=N82E16820104571')
+applications.', 'http://www.newegg.com/Product/Product.aspx?Item=N82E16820104571');
 INSERT INTO memory(name, img_url, store, price, description, buy_link) VALUES ('HyperX Fury 16GB', 
-'http://i.imgur.com/Bv4LITa.jpg', 'Amazon', '60', 'Double the standard desktop memory size. If you don\'t know if you need this much 
-memory, you probably don\'t.', 'http://www.amazon.com/dp/B00J8E8Y5C/?tag=pcpapi-20')
+'http://i.imgur.com/Bv4LITa.jpg', 'Amazon', '60', 'Double the standard desktop memory size. If you don''t know if you need this much 
+memory, you probably don''t.', 'http://www.amazon.com/dp/B00J8E8Y5C/?tag=pcpapi-20');
 
 -- HD's
 INSERT INTO hds(name, img_url, store, price, description, buy_link) VALUES ('Western Digital Caviar Blue 1TB', 
 'http://www.storagereview.com/images/WDC-Caviar-Blue.jpg', 'OutletPC', '49', '1 terabyte of storage is more than enough for 99% of cases.
-You should never run into a problem.', 'http://www.outletpc.com/dx5854-western-digital-caviar-blue-1tb.html?utm_source=dx5854-western-digital-caviar-blue-1tb&utm_medium=shopping%2Bengine&utm_campaign=pcpartpicker&utm_content=Western%20Digital%2B-%2BHard%20Drives%20%26%20SSD%20%3E%203%2E5%22%20SATA%20Hard%20Drives')
+You should never run into a problem.', 'http://www.outletpc.com/dx5854-western-digital-caviar-blue-1tb.html?utm_source=dx5854-western-digital-caviar-blue-1tb&utm_medium=shopping%2Bengine&utm_campaign=pcpartpicker&utm_content=Western%20Digital%2B-%2BHard%20Drives%20%26%20SSD%20%3E%203%2E5%22%20SATA%20Hard%20Drives');
 INSERT INTO hds(name, img_url, store, price, description, buy_link) VALUES ('Western Digital Caviar Red 3TB', 
-'http://www.gts.jo/sites/default/files/3TB%202_0.jpg', 'Amazon', '107', 'Once again, if you don\'t know if you need this much storage you don\'t. 3
-terabytes is an insane amount of storage.', 'http://www.amazon.com/dp/B008JJLW4M/?tag=pcpapi-20')
+'http://www.gts.jo/sites/default/files/3TB%202_0.jpg', 'Amazon', '107', 'Once again, if you don''t know if you need this much storage you don''t. 3
+terabytes is an insane amount of storage.', 'http://www.amazon.com/dp/B008JJLW4M/?tag=pcpapi-20');
 
 -- GPU's
 INSERT INTO gpus(name, img_url, store, price, description, buy_link) VALUES ('EVGA GTX 750 Ti 2GB Superclocked', 
 'http://images10.newegg.com/ProductImage/14-487-025-16.jpg', 'Newegg', '100', 'Probably THE best graphics card for a budget. Handles 
-everything flawlessly up to some light gaming', 'http://www.newegg.com/Product/Product.aspx?Item=N82E16814487024&cm_re=evga_gtx_750_ti-_-14-487-024-_-Product')
+everything flawlessly up to some light gaming', 'http://www.newegg.com/Product/Product.aspx?Item=N82E16814487024&cm_re=evga_gtx_750_ti-_-14-487-024-_-Product');
 INSERT INTO gpus(name, img_url, store, price, description, buy_link) VALUES ('Gigabyte GTX 970 4GB WINDFORCE', 
 'http://media.bestofmicro.com/J/Q/454310/gallery/Gigabyte-GTX-970-Windforce-OC-View-ISO_w_600.jpg', 'Newegg', '305', 'One of the fastest graphics cards on the market. 
-Top tier speed for ALL gaming for a decent price', 'http://www.newegg.com/Product/Product.aspx?Item=N82E16814125684&nm_mc=AFC-C8Junction&cm_mmc=AFC-C8Junction-PCPartPicker,%20LLC-_-na-_-na-_-na&cm_sp=&AID=10446076&PID=3938566&SID=')
+Top tier speed for ALL gaming for a decent price', 'http://www.newegg.com/Product/Product.aspx?Item=N82E16814125685');
 INSERT INTO gpus(name, img_url, store, price, description, buy_link) VALUES ('MSI GTX 980 4GB Twin Frozr', 
 'https://www.komplett.no/img/p/800/80ce0942-1911-4c61-9703-4970d8e5d20c.jpg', 'B&H', '468', 'Unbelievably powerful card with a price point to match. 
-if you want to go all-in, this is your flashiest purchase.', 'http://www.bhphotovideo.com/c/product/1086385-REG/msi_gtx_980_gaming_4g_gforce_gtx980_gaming_graphics.html')
+if you want to go all-in, this is your flashiest purchase.', 'http://www.bhphotovideo.com/c/product/1086385-REG/msi_gtx_980_gaming_4g_gforce_gtx980_gaming_graphics.html');
 
 -- MOTHERBOARDS
 INSERT INTO motherboards(name, img_url, store, price, description, buy_link) VALUES ('MSI H81M-P33 Micro ATX', 
 'http://ecx.images-amazon.com/images/I/71zaOieS0AL._SL1000_.jpg', 'OutletPC', '45', 'Absolutely solid motherboard with all the basic 
-features you can ask for.', 'http://www.outletpc.com/ub7585-msi-h81m-p33-intel-lga-1150-microatx-motherboard.html?utm_source=ub7585-msi-h81m-p33-intel-lga-1150-microatx-motherboard&utm_medium=shopping%2Bengine&utm_campaign=pcpartpicker&utm_content=MSI%2B-%2BMotherboards%20%3E%20Intel%20LGA1150')
+features you can ask for.', 'http://goo.gl/lAOXXm');
 INSERT INTO motherboards(name, img_url, store, price, description, buy_link) VALUES ('ASUS M5A97 ATX', 
 'https://images-na.ssl-images-amazon.com/images/I/5131NAECBkL.jpg', 'OutletPC', '93', 'Essentially functionality to the lower tier here, with a few 
-potential bonus features.', 'http://www.outletpc.com/dc2172-asus-m5a97-r2-0-am3-970-atx-motherboard.html?utm_source=dc2172-asus-m5a97-r2-0-am3-970-atx-motherboard&utm_medium=shopping%2Bengine&utm_campaign=pcpartpicker&utm_content=Asus%2B-%2BMotherboards%20%3E%20AMD%20Socket%20AM3')
+potential bonus features.', 'http://goo.gl/vn5GsE');
 
 -- PSU's
 INSERT INTO psus(name, img_url, store, price, description, buy_link) VALUES ('EVGA 500B', 
-'http://www.evga.com/products/images/gallery/100-B1-0500-KR_XL_1.jpg', 'Newegg', '30', 'Bronze-level efficiency. 500 Watts.', 'http://www.newegg.com/Product/Product.aspx?Item=N82E16817438012&nm_mc=AFC-C8Junction&cm_mmc=AFC-C8Junction-PCPartPicker,%20LLC-_-na-_-na-_-na&cm_sp=&AID=10446076&PID=3938566&SID=')
+'http://www.evga.com/products/images/gallery/100-B1-0500-KR_XL_1.jpg', 'Newegg', '30', 'Bronze-level efficiency. 500 Watts.', 'http://www.newegg.com/Product/Product.aspx?Item=N82E16817438012&nm_mc=AFC-C8Junction&cm_mmc=AFC-C8Junction-PCPartPicker,%20LLC-_-na-_-na-_-na&cm_sp=&AID=10446076&PID=3938566&SID=');
 INSERT INTO psus(name, img_url, store, price, description, buy_link) VALUES ('Corsair CX 600W', 
-'http://www.corsair.com/Media/catalog/product/c/x/cxm600_sideview_a.png', 'Newegg', '45', 'Bronze-level efficiency. 600 Watts.', 'http://www.newegg.com/Product/Product.aspx?Item=N82E16817139048&nm_mc=AFC-C8Junction&cm_mmc=AFC-C8Junction-PCPartPicker,%20LLC-_-na-_-na-_-na&cm_sp=&AID=10446076&PID=3938566&SID=')
+'http://www.corsair.com/Media/catalog/product/c/x/cxm600_sideview_a.png', 'Newegg', '45', 'Bronze-level efficiency. 600 Watts.', 'http://www.newegg.com/Product/Product.aspx?Item=N82E16817139048&nm_mc=AFC-C8Junction&cm_mmc=AFC-C8Junction-PCPartPicker,%20LLC-_-na-_-na-_-na&cm_sp=&AID=10446076&PID=3938566&SID=');
 INSERT INTO psus(name, img_url, store, price, description, buy_link) VALUES ('Corsair 760W 80+ Platinum', 
 'http://www.corsair.com/~/media/Corsair/Product%20Photos/psu/ax-series/ax760/large/AX760_PSU_sideview_a.png', 'Amazon', '140', 'Platinum-level efficiency. 760 Watts. This one will last you the longest - if you really want to spend the extra 
-buck it isn\'t the worst idea.', 'http://www.amazon.com/dp/B00A0HZMEM/?tag=pcpapi-20')
+buck it isn''t the worst idea.', 'http://www.amazon.com/dp/B00A0HZMEM/?tag=pcpapi-20');
 
 -- CASES 
 INSERT INTO cases(name, img_url, store, price, description, buy_link) VALUES ('Corsair 200R', 
 'http://www.corsair.com/~/media/Corsair/Product-Photo-Root/2/200r_hero_up.png', 'MacMall', '56', 
-'A very standard and well-made case. The best choice for a subtle case that just works.', 'http://www.macmall.com/p/8052930?dpno=9436308&source=zwb12166')
+'A very standard and well-made case. The best choice for a subtle case that just works.', 'http://www.macmall.com/p/8052930?dpno=9436308&source=zwb12166');
 INSERT INTO cases(name, img_url, store, price, description, buy_link) VALUES ('Fractal R4 Black Pearl w/ Window', 
 'http://content.hwigroup.net/images/products_xl/168603/fractal-design-define-r4-window-black-pearl.jpg', 'NCIXUS', '80', 
-'One of the most popular cases on the market. The Fractal R4 is subtle, sleek, and built exceptionally well.', 'http://www.ncixus.com/products/?usaffiliateid=1000031504&sku=75045&vpn=FD-CA-DEF-R4-BL-W&manufacture=Fractal%20Design&promoid=1057')
+'One of the most popular cases on the market. The Fractal R4 is subtle, sleek, and built exceptionally well.', 'http://www.ncixus.com/products/?usaffiliateid=1000031504&sku=75045&vpn=FD-CA-DEF-R4-BL-W&manufacture=Fractal%20Design&promoid=1057');
 INSERT INTO cases(name, img_url, store, price, description, buy_link) VALUES ('Corsair 600T White Graphite', 
 'http://cwsmgmt.corsair.com/media/catalog/product/6/0/600tw_heroa_glass_b_3.png', 'Amazon', '187', 
-'Go big or go home. This beast of a case will impress.', 'http://www.amazon.com/dp/B004O0PAKW/?tag=pcpapi-20')
+'Go big or go home. This beast of a case will impress.', 'http://www.amazon.com/dp/B004O0PAKW/?tag=pcpapi-20');
 
 -- MONITORS
 INSERT INTO monitors(name, img_url, store, price, description, buy_link) VALUES ('ASUS VX238 23.0"', 
 'http://images17.newegg.com/is/image/newegg/24-236-305-TS?$S640W$', 'Micro Center', '130', 
-'A go-to budget monitor. Not the best, not the worst.', 'http://www.microcenter.com/product/406703/VX238H_23_Widescreen_Full_HD_1080p_LED_Monitor')
+'A go-to budget monitor. Not the best, not the worst.', 'http://www.microcenter.com/product/406703/VX238H_23_Widescreen_Full_HD_1080p_LED_Monitor');
 INSERT INTO monitors(name, img_url, store, price, description, buy_link) VALUES ('Asus VG248QE 144Hz 24.0"', 
 'http://www.techpowerup.com/img/12-11-27/227a.jpg', 'Micro Center', '240', 
-'The ultimate go-to monitor: easily one of if not THE most popular monitor on the market. Crisp, customizable, and affordable.', 'http://www.microcenter.com/product/409464/VG248QE_24_1080p_144Hz_3D_Gaming_LED_Monitor')
+'The ultimate go-to monitor: easily one of if not THE most popular monitor on the market. Crisp, customizable, and affordable.', 'http://www.microcenter.com/product/409464/VG248QE_24_1080p_144Hz_3D_Gaming_LED_Monitor');
 INSERT INTO monitors(name, img_url, store, price, description, buy_link) VALUES ('Acer MX299Q 29.0"', 
 'https://www.asus.com/media/global/products/kBouhgw8NltyDeag/DAlKlIMOyS0Tl8zE_500.jpg', 'Amazon', '369', 
-'Oh you fancy, huh? While this monitor doesn\'t have the speed of the VG248QE, it\'s size and beauty make up for it', 'http://www.amazon.com/dp/B00ENCTVDS/?tag=pcpapi-20')
+'Oh you fancy, huh? While this monitor doesn''t have the speed of the VG248QE, it''s size and beauty make up for it', 'http://www.amazon.com/dp/B00ENCTVDS/?tag=pcpapi-20');
 
 -- HEADPHONES 
 INSERT INTO headphones(name, img_url, store, price, description, buy_link) VALUES ('SteelSeries Siberia V3', 
 'http://ecx.images-amazon.com/images/I/51QlcyOdvyL._SY355_.jpg', 'Amazon', '60', 
-'Comfortable, high-quality, affordable. Subtle black.', 'http://www.amazon.com/SteelSeries-Siberia-Comfortable-Gaming-Headset/dp/B00O06ZMRG')
+'Comfortable, high-quality, affordable. Subtle black.', 'http://www.amazon.com/SteelSeries-Siberia-Comfortable-Gaming-Headset/dp/B00O06ZMRG');
 INSERT INTO headphones(name, img_url, store, price, description, buy_link) VALUES ('Sennheiser HD 558', 
 'http://en-uk.sennheiser.com/images/713/all/square/6008/square_louped_hd_558_01_sq_sennheiser.png', 'Amazon', '100', 
-'Sennheiser is synonymous with high-quality, comfortable headphones. You won\'t regret spending the extra money on these.', 'http://www.amazon.com/dp/B004FEEY9A/?tag=pcpapi-20')
+'Sennheiser is synonymous with high-quality, comfortable headphones. You won''t regret spending the extra money on these.', 'http://www.amazon.com/dp/B004FEEY9A/?tag=pcpapi-20');
 
 -- KEYBOARDS
 INSERT INTO keyboards(name, img_url, store, price, description, buy_link) VALUES ('Cooler Master Storm QF Rapid', 
 'http://ecx.images-amazon.com/images/I/61IYKyQkYuL._SX522_.jpg', 'Amazon', '70', 
-'A well-made mechanical keyboard for a budget build. Nothing fancy about it but it works like a charm.', 'http://www.amazon.com/dp/B0068INSUM/?tag=pcpapi-20')
+'A well-made mechanical keyboard for a budget build. Nothing fancy about it but it works like a charm.', 'http://www.amazon.com/dp/B0068INSUM/?tag=pcpapi-20');
 INSERT INTO keyboards(name, img_url, store, price, description, buy_link) VALUES ('Corsair K70 RGB', 
 'http://www.corsair.com/~/media/Corsair/Product%20Photos/keyboards/k70/cg-k70-rgb/large/CGK70RGB_06_B.png', 'B&H', '170', 
 'The flashiest part on this website. Programmable custom lighting effects from rainbows to touch-sensor electic pulses, not only will this work perfectly but it will light up the room - literally. ', 
-'http://www.bhphotovideo.com/c/product/1126943-REG/corsair_ch_9000068_na_k70_rgb_mech_gaming.html')
+'http://www.bhphotovideo.com/c/product/1126943-REG/corsair_ch_9000068_na_k70_rgb_mech_gaming.html');
 INSERT INTO keyboards(name, img_url, store, price, description, buy_link) VALUES ('SteelSeries APEX', 
 'http://www2.pcmag.com/media/images/409959-steelseries-apex.jpg?thumb=y', 'Amazon', '88', 
-'A beautiful high-end keyboard for people who don\'t want something mechanical.', 'http://www.amazon.com/dp/B00DQ5RGOY/?tag=pcpapi-20')
+'A beautiful high-end keyboard for people who don''t want something mechanical.', 'http://www.amazon.com/dp/B00DQ5RGOY/?tag=pcpapi-20');
 INSERT INTO keyboards(name, img_url, store, price, description, buy_link) VALUES ('Logitech K120', 
 'http://www.logitech.com/assets/55113/k120-gallery.png', 'OutletPC', '12', 
 'A keyboard for those who are outraged at the price of the other keyboards. Absolutely nothing special about it, but absolutely nothing wrong with its functionality.', 
-'http://www.outletpc.com/hb8264.html?utm_source=hb8264&utm_medium=shopping%2Bengine&utm_campaign=pcpartpicker&utm_content=Logitech%2B-%2BKeyboards%20%26%20Mice%20%3E%20Keyboards%20-%20Wired')
+'http://www.outletpc.com/hb8264.html?utm_source=hb8264&utm_medium=shopping%2Bengine&utm_campaign=pcpartpicker&utm_content=Logitech%2B-%2BKeyboards%20%26%20Mice%20%3E%20Keyboards%20-%20Wired');
 
 -- MICE
 INSERT INTO mice(name, img_url, store, price, description, buy_link) VALUES ('Microsoft Mobile Mouse 3500', 
 'http://ecx.images-amazon.com/images/I/41LLwM-SMkL._SY300_.jpg', 'Amazon', '15', 
-'A great, cheap all-around wireless mouse for non-gamers.', 'http://www.amazon.com/dp/B0035ERKYW/?tag=pcpapi-20')
+'A great, cheap all-around wireless mouse for non-gamers.', 'http://www.amazon.com/dp/B0035ERKYW/?tag=pcpapi-20');
 INSERT INTO mice(name, img_url, store, price, description, buy_link) VALUES ('Razer DeathAdder 2013', 
 'http://images10.newegg.com/ProductImage/26-153-145-03.jpg', 'Micro Center', '50', 
-'The ultimate high-performance mouse. Extreme precision, perfect for FPS (first person shooter) gamers', 'http://www.microcenter.com/product/406525/Deathadder_2013_Gaming_Mouse')
+'The ultimate high-performance mouse. Extreme precision, perfect for FPS (first person shooter) gamers', 'http://www.microcenter.com/product/406525/Deathadder_2013_Gaming_Mouse');
 INSERT INTO mice(name, img_url, store, price, description, buy_link) VALUES ('Razer Naga 2014', 
 'http://assets.razerzone.com/eeimages/products/13785/razer-naga-2014-right-03.png', 'Best Buy', '50', 
 'A mouse known specifically for its use in MMO (massive multiplayer online) games. 12 side buttons make this mouse the best one like it on the market.', 
-'http://www.bestbuy.com/site/razer-naga-expert-mmo-gaming-mouse-black/1653812.p?id=1219058303220&ref=199&loc=8BacdVP0GFs&acampID=1&siteID=8BacdVP0GFs-_yQ5PJq0Maqt7loyb0Z8FA')
+'http://www.bestbuy.com/site/razer-naga-expert-mmo-gaming-mouse-black/1653812.p?id=1219058303220&ref=199&loc=8BacdVP0GFs&acampID=1&siteID=8BacdVP0GFs-_yQ5PJq0Maqt7loyb0Z8FA');
 
 -- SPEAKERS
 INSERT INTO speakers(name, img_url, store, price, description, buy_link) VALUES ('Logitech Z506', 
 'http://www.logitech.com/assets/55403/2/z506-gallery.png', 'Best Buy', '58', 
-'A perfect place to start for computer speakers. Great bang for the buck, literally.', 'http://www.bestbuy.com/site/logitech-z506-5-1-surround-sound-speakers-6-piece-black/1215025.p?id=1218236333141&ref=199&loc=8BacdVP0GFs&acampID=1&siteID=8BacdVP0GFs-UfJiVPIuKJ863Apx6xPp8w')
+'A perfect place to start for computer speakers. Great bang for the buck, literally.', 'http://www.bestbuy.com/site/logitech-z506-5-1-surround-sound-speakers-6-piece-black/1215025.p?id=1218236333141&ref=199&loc=8BacdVP0GFs&acampID=1&siteID=8BacdVP0GFs-UfJiVPIuKJ863Apx6xPp8w');
 INSERT INTO speakers(name, img_url, store, price, description, buy_link) VALUES ('Logitech Z623', 
 'http://ecx.images-amazon.com/images/I/81RVg9wP%2BbL._SL1500_.jpg', 'Amazon', '120', 
-'You dont need more than this unless you are going clubbing with your PC. Incredible quality and volume for the price.', 'http://www.amazon.com/dp/B003VAHYTG/?tag=pcpapi-20')
+'You dont need more than this unless you are going clubbing with your PC. Incredible quality and volume for the price.', 'http://www.amazon.com/dp/B003VAHYTG/?tag=pcpapi-20');
 
 
 
