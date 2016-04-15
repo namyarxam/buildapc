13e7627b29caa72371be1a23bec89c80b6655d3c
Max Rayman <mxray333@gmail.com>
2016-04-15 14:55:27 -0400

    heroku bug fix part 4

--- db/pg.js
+++ db/pg.js
@@ -6,7 +6,7 @@ if(process.env.NODE_ENV === 'production') {
   var config = process.env.DATABASE_URL || {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_NAME.
+    database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS
   };
