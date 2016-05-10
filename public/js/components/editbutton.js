55a0a03d4ff6610f3dc0a5ee11f3f187e2eee97d
Max Rayman <mxray333@gmail.com>
2016-05-10 17:18:43 -0400

    fixed commenting

--- public/js/components/editbutton.js
+++ public/js/components/editbutton.js
@@ -3,15 +3,10 @@ const React = require('react');
 const EditButton = React.createClass({
   clickHandler: function(e) {
     e.preventDefault();
-    // Update price
     let price = -1 * parseInt($(`${this.props.part}price`));
     this.props.updatePrice(price);
-
-    // Update part list
     $(`.${this.props.part}-toggle`).show();
     this.props.updatePart(this.props.part, '');
-
-    // Show edit form
     this.props.nextStage(this.props.num)
   },
 
